74557b2b7f5ec92f3c4cf9ee5c72ba877c5b377d
SanguineTitan57 <69134964+SanguineTitan57@users.noreply.github.com>
2024-09-28 00:24:58 +0300

    Next JS update, Minor styling changes

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,8 @@ import { ConnectButton } from "@rainbow-me/rainbowkit";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 text-black">
       <ConnectButton/>      
-    </div>
+    </main>
   );
 }
